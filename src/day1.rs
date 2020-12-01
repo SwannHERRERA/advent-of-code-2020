06b9c38917b1a62193ba84beec9026c418762e4e
Swann <swann@devloup.dev>
2020-12-01 18:34:10 +0100

    init + test

---
+++ src/day1.rs
@@ -0,0 +1,13 @@
+fn get_the_two_whose_sum_is_equal_to2020(v: Vec<i64>) {
+
+}
+
+
+#[cfg(test)]
+mod test {
+  #[test]
+  test_get_the_two_whose_sum_is_equal_to2020() {
+    let result = get_the_two_whose_sum_is_equal_to();
+    assert_eq!(result, (1721, 299));
+  }
+}
---
+++ tests/day1.rs
@@ -0,0 +1,6 @@
+#[test]
+fn test_day1() {
+  let expense_report = [1721, 979, 366, 299, 675, 1456];
+  let result = puzzle1(expense_report);
+  assert_eq!(result, 514579);
+}
